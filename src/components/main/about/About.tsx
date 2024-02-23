880dd0be1a2a18a446bf57650261d16c7e96fb75
Mohammad Osman Rasooli <mohammad.osmanrasooli1973@gmail.com>
2024-02-23 16:23:51 +0000

    Changed design in Skill section

--- src/components/main/about/About.tsx
+++ src/components/main/about/About.tsx
@@ -1,37 +1,61 @@
+import { motion } from "framer-motion";
 import SectionHeading from "../../ui/section-heading";
 import { TextGenerateEffect } from "../../ui/text-generate-effect";
 import AnimatedCircleShape from "../../ui/animated-circle-shape";
+
 const About = () => {
   return (
-    <section className="about-me min-h-[500px] pt-5 pb-20 flex flex-col md:flex-row justify-center items-center gap-5 relative overflow-hidden">
+    <section className="about-me min-h-[500px] pt-20 pb-10 flex flex-col md:flex-row justify-center items-start gap-5 relative overflow-hidden">
       <AnimatedCircleShape
         deg={[-10, 16, -10]}
         duration={10}
         color="border-pink-500"
-        className="opacity-20 -left-6 -top-2 -bottom-1"
+        className="opacity-20 -left-6 -top-5 -bottom-1"
       />
       <AnimatedCircleShape
         deg={[-15, 15, -15]}
         duration={8}
         color="border-slate-50"
-        className="opacity-10 -left-5 -top-5 -bottom-5"
+        className="opacity-10 -left-5 -top-8 -bottom-5"
       />
       <AnimatedCircleShape
         deg={[-8, 15, -8]}
         duration={10}
         color="border-slate-100"
-        className="opacity-10 left-2 -top-2 -bottom-2"
+        className="opacity-10 left-2 -top-6 -bottom-2"
       />
 
       <SectionHeading title="About" />
       <div className="px-8 md:px-10 md:pl-8 lg:px-16 lg:pl-12">
-        <p className=" font-seaweed text-slate-400 italic">Hey there,</p>
-        <p className="mt-3 text-slate-400 font-gilroyLight text-xl italic">
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 0.9, y: 0 }}
+          viewport={{ once: true }}
+          transition={{
+            delay: 0.5,
+            duration: 0.5,
+            ease: "easeInOut",
+          }}
+          className=" font-seaweed text-slate-400 italic"
+        >
+          Hey there,
+        </motion.p>
+        <motion.p
+          initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 0.9, y: 0 }}
+          viewport={{ once: true }}
+          transition={{
+            delay: 0.6,
+            duration: 0.5,
+            ease: "easeInOut",
+          }}
+          className="mt-3 text-slate-400 font-gilroyLight text-xl italic"
+        >
           I'm{" "}
           <span className="inline-block ml-2 not-italic font-gilroyBold text-4xl text-violet-600 tracking-wider">
             Osman Rasooli
           </span>
-        </p>
+        </motion.p>
         <TextGenerateEffect words="A dedicated Frontend Developer and UI/UX designer with a passion for crafting seamless digital experiences. With a meticulous eye for detail and a commitment to excellence, I strive to deliver user-centric designs that not only captivate but also elevate." />
       </div>
     </section>
--- src/components/main/skills/Skills.tsx
+++ src/components/main/skills/Skills.tsx
@@ -1,5 +1,6 @@
+import { motion } from "framer-motion";
 import SectionHeading from "../../ui/section-heading";
-const About = () => {
+const Skills = () => {
   const frontend = ["html", "css", "sass", "js", "reactjs", "nextjs"];
   const backend = [
     "php",
@@ -12,53 +13,119 @@ const About = () => {
   ];
   const others = ["wordpress", "figma", "jest", "gulp"];
   return (
-    <section className="skills pt-5 pb-20 flex flex-col md:flex-row justify-center items-center gap-5 relative overflow-hidden">
+    <section className="skills pt-5 pb-20 flex flex-col md:flex-row justify-center items-start gap-5 relative overflow-hidden">
       <SectionHeading title="Skills" />
       <div className="px-8 md:px-10 md:pl-8 lg:px-32 lg:pl-12 w-full lg:ml-10">
         {/* Frontend */}
         <div className="w-full text-slate-100 mb-10">
           <div className="">
-            <h3 className=" text-violet-500 text-4xl mb-5">Frontend</h3>
-            <ul className="flex gap-4 px-10">
+            <motion.h3
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 0.9, y: 0 }}
+              viewport={{ once: true }}
+              transition={{
+                delay: 0.4,
+                duration: 0.5,
+                ease: "easeInOut",
+              }}
+              className=" text-violet-500 text-3xl mb-5 font-gilroyLight"
+            >
+              Frontend
+            </motion.h3>
+            <motion.ul
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 0.9, y: 0 }}
+              viewport={{ once: true }}
+              transition={{
+                delay: 0.6,
+                duration: 0.5,
+                ease: "easeInOut",
+              }}
+              className="flex gap-4 px-10"
+            >
               {frontend.map((fr, idx) => (
                 <li
                   key={fr + idx}
-                  className="bg-slate-300 px-4 py-3 rounded-casual-round bg-opacity-15 text-slate-200 border-[1px] border-violet-600 font-bold uppercase"
+                  className="bg-slate-300 px-4 py-3 rounded-casual-round bg-opacity-5 text-slate-200 border-[1px] border-violet-600 font-bold uppercase"
                 >
                   {fr}
                 </li>
               ))}
-            </ul>
+            </motion.ul>
           </div>
         </div>
         <div className="w-full text-slate-100 mb-10">
           <div className="my-5">
-            <h3 className=" text-violet-500 text-4xl mb-5">Backend</h3>
-            <ul className="flex gap-4 px-10">
+            <motion.h3
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 0.9, y: 0 }}
+              viewport={{ once: true }}
+              transition={{
+                delay: 0.8,
+                duration: 0.5,
+                ease: "easeInOut",
+              }}
+              className=" text-violet-500 text-3xl mb-5 font-gilroyLight"
+            >
+              Backend
+            </motion.h3>
+            <motion.ul
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 0.9, y: 0 }}
+              viewport={{ once: true }}
+              transition={{
+                delay: 1,
+                duration: 0.5,
+                ease: "easeInOut",
+              }}
+              className="flex gap-4 px-10"
+            >
               {backend.map((be, idx) => (
                 <li
                   key={be + idx}
-                  className="bg-slate-300 px-4 py-3 rounded-casual-round bg-opacity-15 text-slate-200 border-[1px] border-violet-600 font-bold uppercase"
+                  className="bg-slate-300 px-4 py-3 rounded-casual-round bg-opacity-5 text-slate-200 border-[1px] border-violet-600 font-bold uppercase"
                 >
                   {be}
                 </li>
               ))}
-            </ul>
+            </motion.ul>
           </div>
         </div>
         <div className="w-full text-slate-100 mb-10">
           <div className="">
-            <h3 className=" text-violet-500 text-4xl mb-5">Other Tools</h3>
-            <ul className="flex gap-4 px-10">
+            <motion.h3
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 0.9, y: 0 }}
+              viewport={{ once: true }}
+              transition={{
+                delay: 1.2,
+                duration: 0.5,
+                ease: "easeInOut",
+              }}
+              className=" text-violet-500 text-3xl mb-5 font-gilroyLight"
+            >
+              Other Tools
+            </motion.h3>
+            <motion.ul
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 0.9, y: 0 }}
+              viewport={{ once: true }}
+              transition={{
+                delay: 1.4,
+                duration: 0.5,
+                ease: "easeInOut",
+              }}
+              className="flex gap-4 px-10"
+            >
               {others.map((ot, idx) => (
                 <li
                   key={ot + idx}
-                  className="bg-slate-300 px-4 py-3 rounded-casual-round bg-opacity-15 text-slate-200 border-[1px] border-violet-600 font-bold uppercase"
+                  className="bg-slate-300 px-4 py-3 rounded-casual-round bg-opacity-5 text-slate-200 border-[1px] border-violet-600 font-bold uppercase"
                 >
                   {ot}
                 </li>
               ))}
-            </ul>
+            </motion.ul>
           </div>
         </div>
       </div>
@@ -66,4 +133,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default Skills;
